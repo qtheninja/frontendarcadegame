e9733e6fba10e7ee305f01fc69d15cb978e5b611
Cody <meanderingmonarch@gmail.com>
2015-07-22 17:42:40 -0500

    Feature: Added player class, attempted to render nothing so far.

--- js/app.js
+++ js/app.js
@@ -25,12 +25,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var player = function(x,y){
+    //you need their location son
+    this.x = x;
+    this.y = y;
+    //THE IMAGE SHEET FOR THE PLAYER
+    this.sprite = 'images/char-boy.png';
+}
+
+player.prototype.update = function() {
+    // hollered at me. 
+    console.log('player was updated');
+}
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+// Draw the player on the screen
+player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
+};
 
 
 // This listens for key presses and sends the keys to your
